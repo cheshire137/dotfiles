149b21a0477f21c871db4afa27dc122283198946
Sarah Vessels <cheshire137@gmail.com>
2016-07-26 12:06:38 -0500

    Add hyperterm-title plugin for hyper term
    Tab titles like iTerm does. See
    https://github.com/parro-it/hyperterm-title

--- hyperterm.js
+++ hyperterm.js
@@ -65,7 +65,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ['hyperterm-title'],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
